003be6ada35613134283603ace55b05685e1633a
oberger <ole.k.berger@t-online.de>
2014-10-19 22:48:44 +0200

    Created the basic MainWindow GUI for the Programmer.

--- Programmer/build-Programmer-Desktop-Debug/ui_mainwindow.h
+++ Programmer/build-Programmer-Desktop-Debug/ui_mainwindow.h
@@ -13,11 +13,17 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSplitter>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,28 +31,131 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
+    QAction *actionOpen;
+    QAction *actionPreferences;
     QWidget *centralWidget;
-    QStatusBar *statusBar;
+    QVBoxLayout *verticalLayout_3;
+    QSplitter *splitter;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout_2;
+    QGroupBox *groupBox;
+    QVBoxLayout *verticalLayout_4;
+    QTextEdit *textEdit;
+    QPushButton *pushButton;
+    QGroupBox *groupBox_2;
+    QVBoxLayout *verticalLayout_5;
+    QWidget *widgetTerminal;
+    QVBoxLayout *verticalLayout;
+    QTextEdit *editOutput;
+    QHBoxLayout *horizontalLayout;
+    QLineEdit *editSendMessage;
+    QPushButton *btnSend;
+    QMenuBar *menuBar;
+    QMenu *menuFile;
+    QMenu *menuEdit;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
-        menuBar = new QMenuBar(MainWindow);
-        menuBar->setObjectName(QStringLiteral("menuBar"));
-        MainWindow->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(MainWindow);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(mainToolBar);
+        MainWindow->resize(424, 399);
+        actionOpen = new QAction(MainWindow);
+        actionOpen->setObjectName(QStringLiteral("actionOpen"));
+        actionPreferences = new QAction(MainWindow);
+        actionPreferences->setObjectName(QStringLiteral("actionPreferences"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        verticalLayout_3 = new QVBoxLayout(centralWidget);
+        verticalLayout_3->setSpacing(6);
+        verticalLayout_3->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
+        splitter = new QSplitter(centralWidget);
+        splitter->setObjectName(QStringLiteral("splitter"));
+        splitter->setOrientation(Qt::Vertical);
+        widget = new QWidget(splitter);
+        widget->setObjectName(QStringLiteral("widget"));
+        verticalLayout_2 = new QVBoxLayout(widget);
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 2);
+        groupBox = new QGroupBox(widget);
+        groupBox->setObjectName(QStringLiteral("groupBox"));
+        verticalLayout_4 = new QVBoxLayout(groupBox);
+        verticalLayout_4->setSpacing(6);
+        verticalLayout_4->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        verticalLayout_4->setContentsMargins(0, 0, 0, 0);
+        textEdit = new QTextEdit(groupBox);
+        textEdit->setObjectName(QStringLiteral("textEdit"));
+
+        verticalLayout_4->addWidget(textEdit);
+
+
+        verticalLayout_2->addWidget(groupBox);
+
+        pushButton = new QPushButton(widget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        verticalLayout_2->addWidget(pushButton);
+
+        splitter->addWidget(widget);
+        groupBox_2 = new QGroupBox(splitter);
+        groupBox_2->setObjectName(QStringLiteral("groupBox_2"));
+        verticalLayout_5 = new QVBoxLayout(groupBox_2);
+        verticalLayout_5->setSpacing(6);
+        verticalLayout_5->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_5->setObjectName(QStringLiteral("verticalLayout_5"));
+        verticalLayout_5->setContentsMargins(0, 0, 0, 0);
+        widgetTerminal = new QWidget(groupBox_2);
+        widgetTerminal->setObjectName(QStringLiteral("widgetTerminal"));
+        verticalLayout = new QVBoxLayout(widgetTerminal);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        editOutput = new QTextEdit(widgetTerminal);
+        editOutput->setObjectName(QStringLiteral("editOutput"));
+
+        verticalLayout->addWidget(editOutput);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        editSendMessage = new QLineEdit(widgetTerminal);
+        editSendMessage->setObjectName(QStringLiteral("editSendMessage"));
+
+        horizontalLayout->addWidget(editSendMessage);
+
+        btnSend = new QPushButton(widgetTerminal);
+        btnSend->setObjectName(QStringLiteral("btnSend"));
+
+        horizontalLayout->addWidget(btnSend);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+
+        verticalLayout_5->addWidget(widgetTerminal);
+
+        splitter->addWidget(groupBox_2);
+
+        verticalLayout_3->addWidget(splitter);
+
         MainWindow->setCentralWidget(centralWidget);
-        statusBar = new QStatusBar(MainWindow);
-        statusBar->setObjectName(QStringLiteral("statusBar"));
-        MainWindow->setStatusBar(statusBar);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 424, 22));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName(QStringLiteral("menuFile"));
+        menuEdit = new QMenu(menuBar);
+        menuEdit->setObjectName(QStringLiteral("menuEdit"));
+        MainWindow->setMenuBar(menuBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuBar->addAction(menuEdit->menuAction());
+        menuFile->addAction(actionOpen);
+        menuEdit->addAction(actionPreferences);
 
         retranslateUi(MainWindow);
 
@@ -56,6 +165,14 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        actionOpen->setText(QApplication::translate("MainWindow", "Open..", 0));
+        actionPreferences->setText(QApplication::translate("MainWindow", "Preferences", 0));
+        groupBox->setTitle(QApplication::translate("MainWindow", "Program", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "Upload", 0));
+        groupBox_2->setTitle(QApplication::translate("MainWindow", "Terminal", 0));
+        btnSend->setText(QApplication::translate("MainWindow", "PushButton", 0));
+        menuFile->setTitle(QApplication::translate("MainWindow", "File", 0));
+        menuEdit->setTitle(QApplication::translate("MainWindow", "Edit", 0));
     } // retranslateUi
 
 };
