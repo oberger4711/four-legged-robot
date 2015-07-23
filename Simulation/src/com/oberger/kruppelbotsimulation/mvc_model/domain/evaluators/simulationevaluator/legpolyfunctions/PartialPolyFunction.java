0597699c3dfeaea4d4352c7ad78f38fe9e2b20be
oberger <ole.k.berger@t-online.de>
2015-07-24 00:37:45 +0200

    Started implementing PartialPolyFunction.

--- Simulation/src/com/oberger/kruppelbotsimulation/mvc_model/domain/evaluators/simulationevaluator/legpolyfunctions/PartialPolyFunction.java
+++ Simulation/src/com/oberger/kruppelbotsimulation/mvc_model/domain/evaluators/simulationevaluator/legpolyfunctions/PartialPolyFunction.java
@@ -8,6 +8,8 @@ package com.oberger.kruppelbotsimulation.mvc_model.domain.evaluators.simulatione
 import com.oberger.kruppelbotsimulation.mvc_model.function.IPolyFunction;
 import com.oberger.kruppelbotsimulation.mvc_model.function.Interpolator;
 import com.oberger.kruppelbotsimulation.util.IReadOnlyVector2;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,30 +18,60 @@ import java.util.List;
  */
 public class PartialPolyFunction implements IPolyFunction {
 
-	private Interpolator interpolator = null;
-	private IReadOnlyVector2 first = null;
-	private List<IReadOnlyVector2> inner = null;
-	private IReadOnlyVector2 last = null;
+    private Interpolator interpolator = null;
+    private IReadOnlyVector2 first = null;
+    private List<IReadOnlyVector2> inner = null;
+    private IReadOnlyVector2 last = null;
 
-	public PartialPolyFunction(IReadOnlyVector2 first, List<IReadOnlyVector2> inner, IReadOnlyVector2 last) {
-		this.first = first;
-		this.inner = inner;
-		this.last = last;
-	}
+    public PartialPolyFunction(IReadOnlyVector2 first, List<IReadOnlyVector2> inner, IReadOnlyVector2 last, Interpolator interpolator) {
+        if  (first == null || inner == null || last == null || interpolator == null) {
+            throw new IllegalArgumentException(new NullPointerException("Passing null is not allowed."));
+        }
+        boolean allOrdered = true;
+        for (int i = 0; i < inner.size(); i++) {
+            IReadOnlyVector2 current = inner.get(i);
+            if (i == 0) {
+                allOrdered &= isOrdered(first, current);
+            }
+            else {
+                allOrdered &= isOrdered(inner.get(i - 1), current);
+            }
+            if (i == inner.size() - 1) {
+                allOrdered &= isOrdered(current, last);
+            }
+        }
+        allOrdered &= isOrdered(first, last);
+        if (!allOrdered) { 
+            throw new IllegalArgumentException("Vectors must be ordered.");
+        }
+        this.first = first;
+        this.inner = inner;
+        this.last = last;
+        this.interpolator = interpolator;
+    }
+    
+    private boolean isOrdered(IReadOnlyVector2 v1, IReadOnlyVector2 v2) {
+        return v2.getX() > v1.getX();
+    }
 
-	@Override
-	public float getValue(float x) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-	}
+    @Override
+    public float getValue(float x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-	@Override
-	public List<IReadOnlyVector2> getPolygons() {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-	}
+    @Override
+    public List<IReadOnlyVector2> getPolygons() {
+        LinkedList<IReadOnlyVector2> polygons = new LinkedList<>();
+        polygons.add(first);
+        polygons.addAll(inner);
+        polygons.add(last);
+        
+        return polygons;
+    }
 
-	@Override
-	public Interpolator getInterpolator() {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-	}
+    @Override
+    public Interpolator getInterpolator() {
+        return interpolator;
+    }
 
 }
--- Simulation/test/com/oberger/kruppelbotsimulation/mvc_model/domain/evaluators/simulationevaluator/legpolyfunctions/PartialPolyFunctionTests.java
+++ Simulation/test/com/oberger/kruppelbotsimulation/mvc_model/domain/evaluators/simulationevaluator/legpolyfunctions/PartialPolyFunctionTests.java
@@ -5,12 +5,19 @@
  */
 package com.oberger.kruppelbotsimulation.mvc_model.domain.evaluators.simulationevaluator.legpolyfunctions;
 
+import com.oberger.kruppelbotsimulation.mvc_model.function.Interpolator;
 import com.oberger.kruppelbotsimulation.util.IReadOnlyVector2;
+import com.oberger.kruppelbotsimulation.util.Vector2;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
 
 /**
  *
@@ -18,19 +25,68 @@ import org.junit.rules.ExpectedException;
  */
 public class PartialPolyFunctionTests {
 
-	@Rule
-	public ExpectedException exception = ExpectedException.none();
-	
-	private PartialPolyFunction createTestee(IReadOnlyVector2 first, List<IReadOnlyVector2> inner, IReadOnlyVector2 last) {
-		return new PartialPolyFunction(first, inner, last);
-	}
-	
-	@Test
-	public void constructor_OnPassFirstNull_ThrowsException() {
-		exception.expect(IllegalArgumentException.class);
-		
-		
-		createTestee(null, new ArrayList<IReadOnlyVector2>(), null);
-	}
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private PartialPolyFunction createTestee(IReadOnlyVector2 first, List<IReadOnlyVector2> inner, IReadOnlyVector2 last, Interpolator interpolator) {
+        return new PartialPolyFunction(first, inner, last, interpolator);
+    }
+
+    private Interpolator createDummyInterpolator() {
+        return Mockito.mock(Interpolator.class);
+    }
+
+    @Test
+    public void constructor_OnPassFirstNull_ThrowsException() {
+        exception.expect(IllegalArgumentException.class);
+
+        createTestee(null, Arrays.asList(new Vector2(1, 2)), new Vector2(2, 2), createDummyInterpolator());
+    }
+
+    @Test
+    public void constructor_OnPassInnerNull_ThrowsException() {
+        exception.expect(IllegalArgumentException.class);
+
+        createTestee(new Vector2(0, 0), null, new Vector2(2, 2), createDummyInterpolator());
+    }
+
+    @Test
+    public void constructor_OnPassLastNull_ThrowsException() {
+        exception.expect(IllegalArgumentException.class);
+
+        createTestee(new Vector2(0, 0), Arrays.asList(new Vector2(1, 2)), null, createDummyInterpolator());
+    }
+
+    @Test
+    public void constructor_OnPassInterpolatorNull_ThrowsException() {
+        exception.expect(IllegalArgumentException.class);
+
+        createTestee(new Vector2(0, 0), Arrays.asList(new Vector2(1, 2)), new Vector2(2, 2), null);
+    }
+    
+    @Test
+    public void constructor_OnOrderInnerFalse_ThrowsException() {
+        exception.expect(IllegalArgumentException.class);
+
+        createTestee(new Vector2(0, 0), Arrays.asList(new Vector2(0, 1)), new Vector2(2, 2), createDummyInterpolator());
+    }
+    
+    @Test
+    public void constructor_OnFirstAndLastFalse_ThrowsException() {
+        exception.expect(IllegalArgumentException.class);
+
+        createTestee(new Vector2(0, 0), new ArrayList<>(), new Vector2(0, 2), createDummyInterpolator());
+    }
+
+    @Test
+    public void getPolygons_OnCall_ReturnsListWithFirstInnerAndLast() {
+        Vector2 first = new Vector2(0, 0);
+        List<IReadOnlyVector2> inner = Arrays.asList(new Vector2(1, 1), new Vector2(2, 2));
+        Vector2 last = new Vector2(3, 3);
+        
+        PartialPolyFunction testee = createTestee(first, inner, last, createDummyInterpolator());
+        
+        Assert.assertThat(testee.getPolygons(), is(equalTo(Arrays.asList(new Vector2(0, 0), new Vector2(1, 1), new Vector2(2, 2), new Vector2(3, 3)))));
+    }
 
 }
