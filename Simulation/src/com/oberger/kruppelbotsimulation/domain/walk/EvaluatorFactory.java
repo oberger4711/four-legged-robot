f2f1e5a2158b3df3eeded7be0ef8a4b665053751
oberger <ole.k.berger@t-online.de>
2015-07-31 23:01:08 +0200

    Implement createSimulationStateEvaluator.
    - A group of evaluators, one for each leg is created.

--- Simulation/src/com/oberger/kruppelbotsimulation/domain/walk/EvaluatorFactory.java
+++ Simulation/src/com/oberger/kruppelbotsimulation/domain/walk/EvaluatorFactory.java
@@ -6,6 +6,7 @@
 package com.oberger.kruppelbotsimulation.domain.walk;
 
 import com.oberger.kruppelbotsimulation.domain.evaluators.simulationevaluator.ISimulationState;
+import com.oberger.kruppelbotsimulation.domain.evaluators.simulationevaluator.LegPosition;
 import com.oberger.kruppelbotsimulation.domain.evaluators.simulationevaluator.Simulation;
 import com.oberger.kruppelbotsimulation.domain.evaluators.simulationevaluator.SimulationEvaluator;
 import com.oberger.kruppelbotsimulation.domain.evaluators.simulationevaluator.SimulationEvaluatorParameters;
@@ -20,41 +21,46 @@ import java.util.List;
  * @author ole
  */
 public class EvaluatorFactory {
-    
+
     private final static int SAMPLES_PER_PERIOD = 40;
-    
+
     private InitWalkStateSettings initSettings = null;
-    
+
     public EvaluatorFactory(InitWalkStateSettings initSettings) {
-        this.initSettings = initSettings;
+	this.initSettings = initSettings;
     }
-    
+
     public IEvaluator<WalkState> createEvaluator() {
-        List<WeightedEvaluator<WalkState>> weightedEvaluators = new ArrayList<>();
-        
-        weightedEvaluators.add(new WeightedEvaluator<>(createRepositionBalancePointEvaluator(), 0.5f));
-        
-        WeightedEvaluatorGroup<WalkState> evaluator = new WeightedEvaluatorGroup<>(weightedEvaluators);
-        
-        return evaluator;
+	List<WeightedEvaluator<WalkState>> weightedEvaluators = new ArrayList<>();
+
+	weightedEvaluators.add(new WeightedEvaluator<>(createRepositionBalancePointEvaluator(), 0.5f));
+
+	WeightedEvaluatorGroup<WalkState> evaluator = new WeightedEvaluatorGroup<>(weightedEvaluators);
+
+	return evaluator;
     }
-    
+
     private IEvaluator<WalkState> createRepositionBalancePointEvaluator() {
-        // TODO:
-        return new WalkStateEvaluatorAdapter(createSimulationEvaluator());
+	return new WalkStateEvaluatorAdapter(createSimulationEvaluator());
     }
-    
+
     private IEvaluator<Simulation> createSimulationEvaluator() {
-        
-        return new SimulationEvaluator(createSimulationEvaluatorParameters(), createSimulationStateEvaluator());
+
+	return new SimulationEvaluator(createSimulationEvaluatorParameters(), createSimulationStateEvaluator());
     }
-    
+
     private SimulationEvaluatorParameters createSimulationEvaluatorParameters() {
-        return new SimulationEvaluatorParameters(initSettings.periodInS, SAMPLES_PER_PERIOD);
+	return new SimulationEvaluatorParameters(initSettings.periodInS, SAMPLES_PER_PERIOD);
     }
-    
+
     private IEvaluator<ISimulationState> createSimulationStateEvaluator() {
-        return null; // TODO
+	List<WeightedEvaluator<ISimulationState>> evaluators = new ArrayList<>();
+	evaluators.add(new WeightedEvaluator<>(new SimulationStateBalancePointEvaluator(LegPosition.BR), 1));
+	evaluators.add(new WeightedEvaluator<>(new SimulationStateBalancePointEvaluator(LegPosition.BL), 1));
+	evaluators.add(new WeightedEvaluator<>(new SimulationStateBalancePointEvaluator(LegPosition.FR), 1));
+	evaluators.add(new WeightedEvaluator<>(new SimulationStateBalancePointEvaluator(LegPosition.FL), 1));
+
+	return new WeightedEvaluatorGroup<>(evaluators);
     }
-    
+
 }
