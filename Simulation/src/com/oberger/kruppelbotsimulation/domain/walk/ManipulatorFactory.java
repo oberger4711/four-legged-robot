97d00f56f6d48dfb19a7e8942e35881692c546aa
oberger <ole.k.berger@t-online.de>
2015-08-03 00:49:00 +0200

    Change manipulation step for x axis.
    - Change manipulation step to float.
    - Change value to time in s context.

--- Simulation/src/com/oberger/kruppelbotsimulation/domain/walk/ManipulatorFactory.java
+++ Simulation/src/com/oberger/kruppelbotsimulation/domain/walk/ManipulatorFactory.java
@@ -11,7 +11,7 @@ import com.oberger.kruppelbotsimulation.domain.evaluators.simulationevaluator.le
 import com.oberger.kruppelbotsimulation.domain.manipulators.ConcatPolyFunctionManipulator;
 import com.oberger.kruppelbotsimulation.domain.manipulators.LegPolyFunctionsManipulator;
 import com.oberger.kruppelbotsimulation.domain.manipulators.PartialPolyFunctionGradientConstraint;
-import com.oberger.kruppelbotsimulation.domain.manipulators.PartialPolyFunctionManipulatorY;
+import com.oberger.kruppelbotsimulation.domain.manipulators.PartialPolyFunctionManipulatorX;
 import com.oberger.kruppelbotsimulation.localsearch.manipulator.ConstrainedManipulator;
 import com.oberger.kruppelbotsimulation.localsearch.manipulator.IConstraint;
 import com.oberger.kruppelbotsimulation.localsearch.manipulator.IManipulator;
@@ -24,33 +24,33 @@ import java.util.List;
  * @author oberger
  */
 public class ManipulatorFactory {
-    
+
     private InitWalkStateSettings settings = null;
-    
+
     public ManipulatorFactory(InitWalkStateSettings settings) {
 	this.settings = settings;
     }
-    
+
     public IManipulator<WalkState> createManipulator() {
 	return new WalkStateManipulatorAdapter(createLegPolyFunctionsManipulator());
     }
-    
+
     private IManipulator<ILegPolyFunctions> createLegPolyFunctionsManipulator() {
 	return new LegPolyFunctionsManipulator(createConcatPolyFunctionManipulator());
     }
-    
+
     private IManipulator<ConcatPolyFunction> createConcatPolyFunctionManipulator() {
 	return new ConcatPolyFunctionManipulator(createPartialPolyFunctionManipulator());
     }
-    
+
     private IManipulator<PartialPolyFunction> createPartialPolyFunctionManipulator() {
 	List<IManipulator<PartialPolyFunction>> manipulators = new ArrayList<>();
 	List<IConstraint<PartialPolyFunction>> constraints = new ArrayList<>();
 	constraints.add(new PartialPolyFunctionGradientConstraint(settings.polygonManipulationMaxGradient));
-	manipulators.add(new ConstrainedManipulator<>(new PartialPolyFunctionManipulatorY(settings.polygonManipulationStep), constraints));
-	manipulators.add(new ConstrainedManipulator<>(new PartialPolyFunctionManipulatorY(-settings.polygonManipulationStep), constraints));
-	
+	manipulators.add(new ConstrainedManipulator<>(new PartialPolyFunctionManipulatorX(settings.polygonManipulationStep), constraints));
+	manipulators.add(new ConstrainedManipulator<>(new PartialPolyFunctionManipulatorX(-settings.polygonManipulationStep), constraints));
+
 	return new ManipulatorGroup<>(manipulators);
     }
-    
+
 }
