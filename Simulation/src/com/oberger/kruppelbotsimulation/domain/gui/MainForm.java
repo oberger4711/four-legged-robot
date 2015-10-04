65d6e5ac0b4fde4be0dfaa881fcaeb1aae76519f
oberger <ole.k.berger@t-online.de>
2015-10-04 12:35:47 +0200

    Add GuiWrapper and draw white back and black border.

--- Simulation/src/com/oberger/kruppelbotsimulation/domain/gui/MainForm.java
+++ Simulation/src/com/oberger/kruppelbotsimulation/domain/gui/MainForm.java
@@ -5,17 +5,17 @@
  */
 package com.oberger.kruppelbotsimulation.domain.gui;
 
+import com.oberger.kruppelbotsimulation.domain.simulation.Simulation;
+
 /**
  *
  * @author oberger
  */
 public class MainForm extends javax.swing.JFrame {
-
-    /**
-     * Creates new form MainForm
-     */
-    public MainForm() {
+    
+    public MainForm(Simulation simulation) {
 	initComponents();
+	simulationView.setSimulation(simulation);
     }
 
     /**
@@ -28,7 +28,7 @@ public class MainForm extends javax.swing.JFrame {
     private void initComponents() {
 
         simulationPanel = new javax.swing.JPanel();
-        simulationView1 = new com.oberger.kruppelbotsimulation.domain.gui.SimulationView();
+        simulationView = new com.oberger.kruppelbotsimulation.domain.gui.SimulationView();
         settingsPanel = new javax.swing.JPanel();
         frameSlider = new javax.swing.JSlider();
         jMenuBar1 = new javax.swing.JMenuBar();
@@ -45,13 +45,13 @@ public class MainForm extends javax.swing.JFrame {
             simulationPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(simulationPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(simulationView1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(simulationView, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         simulationPanelLayout.setVerticalGroup(
             simulationPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(simulationPanelLayout.createSequentialGroup()
-                .addComponent(simulationView1, javax.swing.GroupLayout.DEFAULT_SIZE, 284, Short.MAX_VALUE)
+                .addComponent(simulationView, javax.swing.GroupLayout.DEFAULT_SIZE, 284, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -117,41 +117,6 @@ public class MainForm extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-	/* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-	 * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-	 */
-	try {
-	    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-		if ("Gtk+".equalsIgnoreCase(info.getName())) {
-		    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-		    break;
-		}
-	    }
-	} catch (ClassNotFoundException ex) {
-	    java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	} catch (InstantiationException ex) {
-	    java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	} catch (IllegalAccessException ex) {
-	    java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-	    java.util.logging.Logger.getLogger(MainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-        //</editor-fold>
-
-	/* Create and display the form */
-	java.awt.EventQueue.invokeLater(new Runnable() {
-	    public void run() {
-		new MainForm().setVisible(true);
-	    }
-	});
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JSlider frameSlider;
     private javax.swing.JMenu jMenu1;
@@ -159,6 +124,6 @@ public class MainForm extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JPanel settingsPanel;
     private javax.swing.JPanel simulationPanel;
-    private com.oberger.kruppelbotsimulation.domain.gui.SimulationView simulationView1;
+    private com.oberger.kruppelbotsimulation.domain.gui.SimulationView simulationView;
     // End of variables declaration//GEN-END:variables
 }
--- Simulation/src/com/oberger/kruppelbotsimulation/domain/gui/SimulationView.java
+++ Simulation/src/com/oberger/kruppelbotsimulation/domain/gui/SimulationView.java
@@ -18,21 +18,34 @@ import javax.swing.JComponent;
 public class SimulationView extends JComponent {
     
     private Simulation simulation;
-    
-    
+    private float t;
 
     @Override
     protected void paintComponent(Graphics g) {
 	if (isOpaque()) {
 	    g.setColor(getBackground());
-	    g.fillRect(0, 0, getWidth(), getHeight());
 	}
+	else {
+	    g.setColor(Color.white);
+	}
+	g.fillRect(0, 0, getWidth(), getHeight());
 	
 	// Draw test rectangle.
 	Graphics2D g2d = (Graphics2D)g.create();
-	g2d.setColor(Color.yellow);
+	g2d.setColor(Color.black);
 	g2d.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
 	g2d.dispose();
+    }
+
+    public void setSimulation(Simulation simulation) {
+	this.simulation = simulation;
+	// TODO: check t
+	// TODO: redraw
+    }
+
+    public void setT(float t) {
+	this.t = t;
+	// TODO: redraw
     }
     
 }
