5e856eed38796ba7adbd7f1e45313a98c0e896a7
oberger <ole.k.berger@t-online.de>
2015-10-05 20:04:58 +0200

    Run simulation with max gradient 60.

--- Arduino/LegPolyFunctionFactory.cpp
+++ Arduino/LegPolyFunctionFactory.cpp
@@ -1,4 +1,4 @@
-#include "LegPolyFunctionFactory.h"
+#include "/home/oberger/Projects/Kruppelbot/Arduino/LegPolyFunctionFactory.h"
 
 WrappedPolyFunction* LegPolyFunctionFactory::createBr()
 {
@@ -7,22 +7,22 @@ WrappedPolyFunction* LegPolyFunctionFactory::createBr()
 	polygons[0].x = 0;
 	polygons[0].y = 150;
 	
-	polygons[1].x = 367;
+	polygons[1].x = 477;
 	polygons[1].y = 132;
 	
-	polygons[2].x = 734;
+	polygons[2].x = 764;
 	polygons[2].y = 115;
 	
-	polygons[3].x = 1102;
+	polygons[3].x = 1052;
 	polygons[3].y = 98;
 	
-	polygons[4].x = 1469;
+	polygons[4].x = 1339;
 	polygons[4].y = 81;
 	
-	polygons[5].x = 1756;
+	polygons[5].x = 1996;
 	polygons[5].y = 64;
 	
-	polygons[6].x = 1844;
+	polygons[6].x = 2284;
 	polygons[6].y = 47;
 	
 	polygons[7].x = 2571;
@@ -47,22 +47,22 @@ WrappedPolyFunction* LegPolyFunctionFactory::createBl()
 	polygons[0].x = 0;
 	polygons[0].y = 150;
 	
-	polygons[1].x = 737;
+	polygons[1].x = 527;
 	polygons[1].y = 132;
 	
-	polygons[2].x = 824;
+	polygons[2].x = 814;
 	polygons[2].y = 115;
 	
 	polygons[3].x = 1102;
 	polygons[3].y = 98;
 	
-	polygons[4].x = 1469;
+	polygons[4].x = 1389;
 	polygons[4].y = 81;
 	
-	polygons[5].x = 1836;
+	polygons[5].x = 1996;
 	polygons[5].y = 64;
 	
-	polygons[6].x = 2204;
+	polygons[6].x = 2284;
 	polygons[6].y = 47;
 	
 	polygons[7].x = 2571;
@@ -87,22 +87,22 @@ WrappedPolyFunction* LegPolyFunctionFactory::createFr()
 	polygons[0].x = 0;
 	polygons[0].y = 150;
 	
-	polygons[1].x = 367;
+	polygons[1].x = 287;
 	polygons[1].y = 132;
 	
-	polygons[2].x = 734;
+	polygons[2].x = 574;
 	polygons[2].y = 115;
 	
-	polygons[3].x = 1002;
+	polygons[3].x = 1252;
 	polygons[3].y = 98;
 	
-	polygons[4].x = 1089;
+	polygons[4].x = 1539;
 	polygons[4].y = 81;
 	
-	polygons[5].x = 1176;
+	polygons[5].x = 1826;
 	polygons[5].y = 64;
 	
-	polygons[6].x = 1264;
+	polygons[6].x = 2114;
 	polygons[6].y = 47;
 	
 	polygons[7].x = 2571;
@@ -127,22 +127,22 @@ WrappedPolyFunction* LegPolyFunctionFactory::createFl()
 	polygons[0].x = 0;
 	polygons[0].y = 150;
 	
-	polygons[1].x = 367;
+	polygons[1].x = 287;
 	polygons[1].y = 132;
 	
-	polygons[2].x = 734;
+	polygons[2].x = 574;
 	polygons[2].y = 115;
 	
-	polygons[3].x = 1102;
+	polygons[3].x = 1212;
 	polygons[3].y = 98;
 	
-	polygons[4].x = 1469;
+	polygons[4].x = 1499;
 	polygons[4].y = 81;
 	
-	polygons[5].x = 1836;
+	polygons[5].x = 1786;
 	polygons[5].y = 64;
 	
-	polygons[6].x = 2204;
+	polygons[6].x = 2074;
 	polygons[6].y = 47;
 	
 	polygons[7].x = 2571;
